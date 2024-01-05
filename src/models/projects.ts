7ac2c39ab70a9c506ef8c10de311f65dbb145ebc
CQuarkH <currihuile@gmail.com>
2024-01-05 02:06:39 -0300

    Portfolio v2 bugs solved

--- src/models/projects.ts
+++ src/models/projects.ts
@@ -36,7 +36,7 @@ export const projects: Project[] = [
     description:
       "An app leveraging the SpaceX API to provide launch, rocket, and launchpad details. Built from scratch with Astro and TypeScript, it features hydrated React components and is styled with Tailwind CSS.",
     imageUrl:
-      "https://private-user-images.githubusercontent.com/109247730/294303211-b3b2adc2-f7e8-4347-8074-d097e52d13ae.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDQ0MjcwMTIsIm5iZiI6MTcwNDQyNjcxMiwicGF0aCI6Ii8xMDkyNDc3MzAvMjk0MzAzMjExLWIzYjJhZGMyLWY3ZTgtNDM0Ny04MDc0LWQwOTdlNTJkMTNhZS5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwMTA1JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDEwNVQwMzUxNTJaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0zNzc2ZTZjOTk5NDM0YzFlNjJlMmU5MGI5YWNhMjRmYzMzZTA1NmNlOThlNzhmY2ZhMjMwZDQxYzZmMDdlZjAzJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.WlQQKdt1BbEJ8WH_SeFg3Zw6Qoo0EzqZOXYD6NObG4I",
+      "https://firebasestorage.googleapis.com/v0/b/praisethesale-c921f.appspot.com/o/spacexplorer.png?alt=media&token=dd1686f1-ed57-4ded-8cf1-a203190537ce",
     technologies: [AstroTech, ReactTech, TypeScriptTech, JavaScriptTech],
     link: "https://spacexplorer-cyan.vercel.app/",
   },
